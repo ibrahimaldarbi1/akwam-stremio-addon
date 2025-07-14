8ee63c36878496549a8369247d70d6ea30727c36
Ibrahim <95380061+ibrahimaldarbi1@users.noreply.github.com>
2025-07-14 05:13:05 +0300

    Add files via upload

---
+++ addon.js
@@ -0,0 +1,23 @@
+const { addonBuilder } = require('stremio-addon-sdk');
+const { scrapeList, scrapeMetaDetails, searchAkwam, getStreams } = require('./akwam-scraper');
+const builder = new addonBuilder(require('./manifest.json'));
+
+builder.defineCatalogHandler(async ({ id, type, extra }) => {
+  if (extra?.search) return { metas: await searchAkwam(extra.search, type) };
+  if (id === 'akwam_movies') return { metas: await scrapeList('movie', 1) };
+  if (id === 'akwam_series') return { metas: await scrapeList('series', 1) };
+  if (id === 'new') return { metas: await scrapeList(type, 1) };
+  return { metas: [] };
+});
+
+builder.defineMetaHandler(async ({ type, id }) => {
+  const m = await scrapeMetaDetails(id);
+  return { meta: { id, type, ...m } };
+});
+
+builder.defineStreamHandler(async ({ type, id }) => {
+  const streams = await getStreams(id);
+  return { streams };
+});
+
+module.exports = builder.getInterface();
